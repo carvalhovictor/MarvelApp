3b2679c5190fea9c21357d90808a4ffe351c065d
Victor Carvalho <victor.carvalho@simoninisoftware.com.br>
2021-12-26 15:56:24 -0300

    hotfixes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
--- js/services/marvelAPIService.js
+++ js/services/marvelAPIService.js
@@ -1,7 +1,7 @@
 angular.module('marvelApp').factory('marvelAPI', function ($http, md5, config) {
     const ts = new Date().getTime()
     const hash = md5.createHash(ts + config.privateKey + config.publicKey).toString();
-    const params = "?ts=" + ts + "&apikey=" + config.publicKey + "&hash=" + hash + "&limit=100" + "&offset=100";
+    const params = "?ts=" + ts + "&apikey=" + config.publicKey + "&hash=" + hash + "&limit=100";
     
     const _getCharacters = function() {
         return $http.get(config.baseUrl + "/characters" + params);
